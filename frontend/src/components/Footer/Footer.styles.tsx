ef0ff9e5b7b83d771ed66f8a53020f8388f68eb4
Tomasz Zawiszowski <zawyszosky@gmail.com>
2025-06-30 09:29:24 +0200

    style: footer

--- frontend/src/components/Footer/Footer.styles.tsx
+++ frontend/src/components/Footer/Footer.styles.tsx
@@ -105,7 +105,9 @@ export const SectionTitle = styled.h6`
   font-weight: bold;
   margin-bottom: 1rem;
   display: flex;
+  justify-content: center;
   align-items: center;
+  
 `;
 
 export const Logo = styled.img`
