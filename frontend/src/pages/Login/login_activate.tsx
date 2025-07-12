751311b6ff8e25dd12bedab3b30b5f4c6c5bce67
Tomasz Zawiszowski <zawyszosky@gmail.com>
2025-07-12 14:49:21 +0200

    feat: activate user from front

--- frontend/src/pages/Login/login_activate.tsx
+++ frontend/src/pages/Login/login_activate.tsx
@@ -1,9 +1,34 @@
+import { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { HandPlatter } from 'lucide-react';
 import { Container, FormWrapper, FormContainer, Header, IconWrapperWait, Title, Subtitle
  } from './login2.styles';
+import { DRF_ADRESS } from '../../data/constants';
+import { useAuthContext } from '../../context/AuthContext';
+import { notify_error, notify_success } from '../../utils/basicToasts';
+
 
 
 const Activate = () => {
+  const {uid, token} = useParams()
+  const navigate = useNavigate()
+  let {config} = useAuthContext();
+
+  useEffect(() => {
+    debugger
+    axios.get(DRF_ADRESS + `/user_api/activate/${uid}/${token}/`, config)
+    .then(_ => {
+      notify_success('Account successfully activated')
+      navigate('/')
+    })
+    .catch(error => {
+      console.log(error)
+      notify_error('Something went wrong')
+    })
+
+
+  },[])
 
 
   return (
